db6a9e98f793591d0d80a5bfa86d6e3b05930e2a
DHarvey84 <djharvey84@gmail.com>
2015-11-20 20:22:31 +0000

    latest updates
    new updates with earth rise

--- moonLanding.js
+++ moonLanding.js
@@ -4,6 +4,7 @@ var moonSpeed = 1  ;                        //set moon speed
 var skyTrackSpeed = 0.25;                    //set the speed stars track across the sky
 var starMap = [] ;                          //initialise Array
 var landImage    ;                           //initialise variable for land image
+var earthImage
 var starSize = 5;                             //set star size
 var micInput     ;                               //initialise variable for microphone input
 var starPositionX = 0 ;                //intitialise star X variable
@@ -11,18 +12,26 @@ var starPositionY = 0 ;                //intitialise star Y variable
 var mp3_1;                                //initialise mp3 variable
 var mp3_2;                                //initialise mp3 variable
 var mp3_3;                                //initialise mp3 variable
-
+var earthX;                                 //initialise earthX
+var earthY;                                 //initialise earthY
 
 //initialise Moon:    
 var moonPositionX = -200 ;
 var moonPositionY = 400;
 
+//initialise Earth
+earthX = 30;
+earthY = 30;
+var earthSize=150;
+    
 function preload(){
         //load MP3
     mp3_1 = loadSound('assets/bensound-scifi.mp3');
     mp3_2 = loadSound('assets/bensound-relaxing.mp3');
     mp3_3 = loadSound('assets/bensound-acousticbreeze.mp3');
 }
+
+
 
 function setup() {
      createCanvas(1300, 800) ;          //set canvas
@@ -37,17 +46,13 @@ function setup() {
      //print out the array of available sources
      console.log(sourceList);
      //set the source to the relevant item in the set
-     micInput.setSource(0);
+        
+     micInput.setSource(1);   //defaults to microphone
+        
     });
     
     //start
     micInput.start();
-    
-
-
-    
-    
-  
     
     //create the initial star map:
     
@@ -62,9 +67,11 @@ function setup() {
         
     }
     
-    //load the moon image:
-    
+    //load the moon image:    
     landImage = loadImage("assets/Landing2.jpg");
+    
+    //load earth image
+    earthImage = loadImage("assets/Earth.png");
 
 }
            
@@ -77,29 +84,32 @@ function draw() {
   noStroke();
   
   //get mic level before frame is drawn
-  soundLevel = micInput.getLevel();
-  
-   
-  
+  soundLevel = micInput.getLevel(0.5);
+    
   //Draw the array of stars:
-  for(i = 0; i < (numberOfStars * 2); i = i + 2){    
-    fill(random(255,180), random(255,180), random(250,(250*soundLevel)));       //set a random star colour also influenced by audio level
-    
-   var TempstarSize = random(starSize,starSize + 1);                     // random star size shimmer effect
-                                                                                //add extra bit of size based on audio soundLevel
+  for(i = 0; i < (numberOfStars * 2); i = i + 2){   
+      
+    //set a random star colour also influenced by audio level
+    fill(random(255,180), random(255,180), random(250,(250*soundLevel)));  
+      
+    // random star size shimmer effect (for during silence)
+   var TempstarSize = random(starSize,starSize + 1);                  
+      
+   //add extra bit of size based on audio soundLevel                                                                             
    TempstarSize = TempstarSize + (15*soundLevel);
-   
-    ellipse(starMap[i],starMap[i+1],TempstarSize,TempstarSize);                      // Draw with the randomised fill and size for shimmer effect
+      
+   // Draw with the randomised fill and size for shimmer effect
+    ellipse(starMap[i],starMap[i+1],TempstarSize,TempstarSize);                      
     
-    
-    starMap[i] = starMap[i] + skyTrackSpeed;                                     //Move the star X position by the start tracking speed
-
-    if (starMap[i] >= width) {                                                      //reset star X position to 0 and random height if it leaves the canvas
+    //Move the star X position by the start tracking speed
+    starMap[i] = starMap[i] + skyTrackSpeed;                                     
+ //reset star X position to 0 and random height if it leaves the canvas
+    if (starMap[i] >= width) {                                                      
                 starMap[i] = 0 ;
                 starMap[i+1]= random(height/1.75);
     }                   
      
-
+//end of star drawing
  }
 
     
@@ -107,16 +117,30 @@ function draw() {
     imageMode(CORNERS);
     image(landImage,0,(height/1.75),width,height);
     
+    //draw earth
+    imageMode(CORNERS);
+    image(earthImage,earthX,earthY,earthX+earthSize,earthY+earthSize);  
+    
+    //move earth
+    earthX = earthX+0.2;
+    earthY = earthY+0.2;
+
+        //reset earth once it has completed an orbit:
+    if (earthX > (width * 1.3)) {
+        earthX = -300;
+        earthY = -300;
+        
+        }
+    
+    
     //draw mooon and move each frame, size dependant on Mic input
-    
-    fill(200,200,255);
+    fill(255,255,255*soundLevel);
     ellipse(moonPositionX,moonPositionY,(75 - (soundLevel*70)),(75 - (soundLevel*70)));
-    //draw moon core
-    fill(0,0,0);
-    ellipse(moonPositionX,moonPositionY,(25 + (soundLevel*50)),(25 + (soundLevel*50)));
+    //draw moon internal core
+    fill(255*soundLevel,255*soundLevel,200);
+    ellipse(moonPositionX,moonPositionY,(45 + (soundLevel*25)),(45 + (soundLevel*25)));
     
-    
-    
+    //move the moon by moon speed and rise
     moonPositionX = moonPositionX + moonSpeed;
     moonPositionY = moonPositionY - 0.15;
     
@@ -124,17 +148,15 @@ function draw() {
     if (moonPositionX > (width * 1.3)) {
         moonPositionX = -300;
         moonPositionY = 400;
+        }
     
-    }
-    
-    //add the credit to bensound for mp3 as stated in license
+    //add the music credit to bensound for mp3 as stated in license
     fill(255,0,255);
     textSize(12);
     text("Music by www.bensound.com",10,height-20);
     
     
-    //find user clicks and play associated MP3
-    
+    //find user clicks and play associated MP3    
     if (mouseIsPressed){
     
         //if user clicks on the left rover
@@ -176,9 +198,10 @@ function draw() {
                 mp3_1.stop();
             
             }
-       
-        }
+          }
+        
+//end of user clicks
     }
     
-
+//end of draw()
 }
